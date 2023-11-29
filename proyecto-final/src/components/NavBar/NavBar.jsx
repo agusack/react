bca23f43a2c761b38199e4554ec7f7f59ac02fcd
Agustín Ackerman <138171336+agusack@users.noreply.github.com>
2023-11-29 03:01:12 -0300

    add navbar
    add NavBar.jsx and NavBar.css

---
+++ proyecto-final/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(props) {
+    return (
+        <div>
+            <h1></h1>
+            <ul className='nav'>
+                <a href="#" >Superheroes</a>
+                <a href="#" >Drama</a>
+                <a href="#" >Accion</a>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
