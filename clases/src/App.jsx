4be25a4dfe91b29cd5826da4218c5f3fcd86fef9
Agustín Ackerman <138171336+agusack@users.noreply.github.com>
2023-12-02 00:28:15 -0300

    clase 5
    clase 5 componentes II

--- clases/src/App.jsx
+++ clases/src/App.jsx
@@ -5,7 +5,45 @@ import NavBar from './components/NavBar/NavBar';
 import Button from './components/Button';
 import Box from './components/Box';
 import Input from './components/Input/index';
+import { Component } from 'react';
+import Timer from './components/ConteinerTimer/index';
+import Counter from './components/Counter';
 
+function SuperButton({ buttonText }) {
+  function doSomething() {
+    console.log('Hello coders!');
+  }
+  return <button type='button' onClick={doSomething}>{ buttonText }</button>
+}
+
+function SuperForm(props) {
+  console.log(props);
+  return (
+    <>
+      <h1>{props.tittle}</h1>
+      {props.boton({ buttonText: 'SuperForm Button' })}    
+    </>
+  );
+}
+
+class ConteinerTimer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showTimer: true
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState({ showTimer: false })}>Destroy Timer</button>
+        { this.state.showTimer && <Timer /> }
+      </div>
+    )
+  }
+}
 
 function App() {
 
@@ -31,8 +69,10 @@ function App() {
       <Button color='yellow'>
         <p>Esto es un children</p>
       </Button>
+      <Counter />
       <Box titulo='Caja Contenedora' />
       <Input handleExtraer={extraerValor} />
+      <SuperForm tittle='Hey!' boton={SuperButton} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ clases/src/components/ConteinerTimer/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class Timer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            seconds: 0
+        }
+    }
+
+    myInterval = setInterval(() => {
+        this.setState(prevState => ({ seconds: prevState.seconds + 1 }))
+    }, 1000);
+
+    componentDidMount() {
+        console.log('Mounted');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('updated');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state.seconds === 10;
+    }
+
+    componentWillUnmount() {
+        console.log('Unmounted');
+    }
+
+    render() {
+        return (
+            <div>Timer: {this.state.seconds} sec</div>
+        )
+    }
+}
